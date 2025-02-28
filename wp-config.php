cf7ea131ae0ad9d3e94e5330ce404979f59f7fed
Zhodiyn <Killemajor31@gmail.com>
2025-02-28 13:08:48 +0400

    Add files via upload

---
+++ wp-config.php
@@ -0,0 +1,24 @@
+
+<?php
+define('DB_NAME', 'blog');
+define('DB_USER', 'blog');
+define('DB_PASSWORD', '3cb2ZE9sTHMmBdRc');
+define('DB_HOST', '46.105.102.52');
+define('DB_CHARSET', 'utf8');
+define('DB_COLLATE', '');
+define('WP_CACHE', true);
+define('AUTH_KEY',         '1@>1-O$$.;kvdKz_SxVt*(+<jQUiGbGy)H@z;-p,{v+#1s?/Ym1|zgM_/1{SG@f[');
+define('SECURE_AUTH_KEY',  't{5}:R9btWS$%j|,JHg8+<vatouvl:jtEar2bB]lKc.vL6<-kC)4[{|GnFI5iM2u');
+define('LOGGED_IN_KEY',    'A[m~52$yAB[-waE;3/<3KtV<~<$M)+zkwD]M*!C<e J:G[B34v.lO]l.nEWOWQiP');
+define('NONCE_KEY',        'qi|rf#k]|]ysbax@Z.GME`2mW/(f7Pzy>]$SC<-#*bop[!`H:2iF-ZG.4Z][&Z6W');
+define('AUTH_SALT',        'a5dh`07+0d7N8|J.X?(y!Uu/^q5CK<<AZ(xL:m+@#Vt8<F*}^8MgCHp@-;iK4u%=');
+define('SECURE_AUTH_SALT', '9fPhJpNrY1,Ci+!2EVm+c.1z)Et;{(3&8~x]iwd.8+gMk!V]?.>SCi>QH6n2H<{C');
+define('LOGGED_IN_SALT',   ':Q7 [LPtdhC)>G&R#+h`&07Q am-u2Z-/V G+x|{?b>W`prp b9$JvAP1-cyEsan');
+define('NONCE_SALT',       '7(kvnX^:U-|#]Jj||QSF-~m6.xpDZWFfqo4?y^r<?M3mWTLv$?;;${R`Yu~c3]+i');
+$table_prefix  = 'wp_';
+define('WPLANG', '');
+define('CONCATENATE_SCRIPTS', false);
+define('WP_DEBUG', false);
+if ( !defined('ABSPATH') )
+	define('ABSPATH', dirname(__FILE__) . '/');
+require_once(ABSPATH . 'wp-settings.php');
